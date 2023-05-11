91150ead25a406e63f2c9aa3b10a64e7e3c47c56
Viktor Volkov <viktor.volkov@C02DV67J07F1.local>
2023-05-11 15:39:08 +0300

    MY-5051-module-ord https://docs.google.com/document/d/1WNDPzGEs-zgsTvoQ6KS3KyP4F_7il94_DWujKc0PkJ0/edit#

---
+++ basic/modules/ord/Module.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace app\modules\ord;
+
+class Module
+{
+    public $controllerNamespace = 'app\modules\ord\controllers';
+
+    public function init()
+    {
+        parent::init();
+    }
+}
